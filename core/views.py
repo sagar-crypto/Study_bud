a2160f1eaaba6e8b5f894cc75329d67a23992e05
Sagar Sikdar <sagarsikdar2812@gmail.com>
2023-11-18 22:11:04 +0100

    adding new routes

--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(requests):
     return render(requests, 'core/index.html')
+
+def contact(requests):
+    return render(requests, 'core/contact.html')
