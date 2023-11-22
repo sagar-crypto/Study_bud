c9b26da4130019d126d16e67e1d82d876bfda02f
Sagar Sikdar <sagarsikdar2812@gmail.com>
2023-11-22 12:23:03 +0100

    added new views and templates for app

--- core/views.py
+++ core/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from item.models import Category, Item
+from .forms import SignupForm
 
 def index(requests):
-    return render(requests, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(requests, 'core/index.html',{
+        'categories': categories,
+        'items': items,
+    })
 
 def contact(requests):
     return render(requests, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+    
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
--- newproject/settings.py
+++ newproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "newproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'core/templates/core'],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
