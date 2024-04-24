40d19ea93a4ab7c698dce8b2dde5e3353ae2d798
Sagar Sikdar <sagarsikdar2812@gmail.com>
2024-04-24 23:10:45 +0200

    new pages for good res

--- studyBud/base/views.py
+++ studyBud/base/views.py
@@ -64,7 +64,7 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__icontains = q) |
                                 Q(name__icontains = q) |
                                 Q(description__icontains = q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     room_count = rooms.count()
 
     room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
@@ -174,3 +174,7 @@ def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     topics = Topic.objects.filter(name__icontains = q)
     return render(request, 'base/topics.html', {'topics' :topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html',{'room_messages': room_messages})
